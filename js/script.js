b98d05248233d36225516701c29020292dcafc30
Dmitry Krutkin <krutkin@a2design.biz>
2023-04-01 02:11:02 +0600

    Layout

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(() => {
+    $('.owl-carousel').owlCarousel({
+        items: 1,
+        margin: 0,
+        loop: true,
+        center: true,
+        nav: true
+    })
+
+    $('.section-col__image img').removeClass('hidden')
+})
