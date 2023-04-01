f37f94090adcfc731fd0e5093277fe60e19987f6
Dmitry Krutkin <krutkin@a2design.biz>
2023-04-01 15:03:00 +0600

    Added age in profile

--- js/script.js
+++ js/script.js
@@ -1,4 +1,13 @@
 $(document).ready(() => {
+    (function calcAge() {
+        const birthdate = new Date(2000, 7, 25) // YYYY, MM, DD
+        const today = new Date()
+        const age = today.getFullYear() - birthdate.getFullYear() -
+            (today.getMonth() < birthdate.getMonth() ||
+                (today.getMonth() === birthdate.getMonth() && today.getDate() < birthdate.getDate()))
+        $('#age').html(age)
+    })()
+
     $('.owl-carousel').owlCarousel({
         items: 1,
         margin: 0,
